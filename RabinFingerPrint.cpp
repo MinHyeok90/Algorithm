abfcce190dc689a4dd7c6c71d78e4020ef70da9b
Minhyuk <dufflaa@gmail.com>
2021-11-27 12:29:45 +0900

    Add documents

--- RabinFingerPrint.cpp
+++ RabinFingerPrint.cpp
@@ -1,9 +1,23 @@
 #include<stdio.h>
-
+/*
 int p = 127, mod = 1610612741;
 int hash(char c[]) {
     long long ans = 0;
     for (int i = 0; c[i]; i++) ans = (p * ans + c[i]) % mod;
+    return ans;
+}
+
+int isSame(char a[], char b[]) {
+    return hash(a) == hash(b);
+}
+*/
+
+int p = 127, mod = 1610612741;
+int hash(char c[]) {
+    long long ans;
+    for (int i = 0; c[i]; i++) {
+        ans = (ans * p + c[i]) % mod;
+    }
     return ans;
 }
 
